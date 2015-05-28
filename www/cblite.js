d22bbc7b7f07c8778a87ea2a955af9ff4d08fc6f
Karl Westin <karl.westin@gmail.com>
2015-05-28 12:39:36 +0200

    Add features that ehealth needs: A 'replicate' method, and a method to listen to events on the database

--- www/cblite.js
+++ www/cblite.js
@@ -6,5 +6,20 @@ module.exports = {
          }, function(err) {
             callback(err);
         }, "CBLite", "getURL", []);
+    },
+    replicate : function(local, remote, callback) {
+         // use node.js style error reporting (first argument)
+         cordova.exec(function(url){
+            callback(false, url);
+         }, function(err) {
+            callback(err);
+        }, "CBLite", "replicate", [local, remote]);
+    },
+    listen: function(dbName, callback) {
+        cordova.exec(function(url){
+          callback(false, url);
+        }, function(err) {
+          callback(err);
+      }, "CBLite", "listen", [dbName]);
     }
-}
+};
